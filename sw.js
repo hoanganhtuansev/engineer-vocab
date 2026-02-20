6b40cceae51a397758f9d794e9591a634995c309
hoanganhtuansev <hoanganhtuansev@gmail.com>
2026-02-21 01:36:32 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Engineer Vocab PWA Service Worker (cache-first) */
-const CACHE_NAME = "engineer-vocab-pwa-v1";
+const CACHE_NAME = "engineer-vocab-pwa-v2";
 const PRECACHE = [
   "./",
   "./index.html",
